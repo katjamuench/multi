5822261422186c17106bf815524095667b4b1c25
katjamuench <katjatmuench@yahoo.com>
2023-08-26 02:29:08 +0200

    CustomCombinedExtractor

--- src/spr_rl/agent/TarMACPolicy.py
+++ src/spr_rl/agent/TarMACPolicy.py
@@ -2,18 +2,11 @@ from typing import Callable, Dict, List, Optional, Tuple, Type, Union
 import numpy as np
 import torch as th
 from torch import nn
-import gym
-from gym import spaces
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from spr_rl.agent.params import Params
 from stable_baselines3.common.policies import ActorCriticPolicy
-from typing import Dict, List, Tuple, Type, Union
-
 import gymnasium as gym
-import torch as th
 from gymnasium import spaces
-from torch import nn
-
 from stable_baselines3.common.preprocessing import get_flattened_obs_dim, is_image_space
 from stable_baselines3.common.type_aliases import TensorDict
 from stable_baselines3.common.utils import get_device
